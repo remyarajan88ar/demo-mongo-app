c376b3636694daf6399644c313afb5068123fe83
remya_rajan88-admin <remya.rajan88ar@gmail.com>
2025-06-03 16:29:13 -0400

    code change

--- src/main/java/io/reflectoring/demo_mongo_app/controller/UserController.java
+++ src/main/java/io/reflectoring/demo_mongo_app/controller/UserController.java
@@ -1,10 +1,31 @@
 package io.reflectoring.demo_mongo_app.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.reflectoring.demo_mongo_app.model.User;
+import io.reflectoring.demo_mongo_app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("")
+    public void addUser(@RequestBody User user){
+        userService.addUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id){
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable String id){
+        userService.deleteUser(id);
+    }
+
 
 }
